f7b8fa084cafd3ab5ca3cfc3202311a16ebbff11
Waruni Lalendra <73272972+WaruniLalendra@users.noreply.github.com>
2021-08-20 01:08:20 -0700

    inputs_and_interrupts (#5)
    idt.s file created.
    interrupts.c and interrupts.h files are created.
    interrupts_handler.s file created.
    pic.c and pic.h files are created.
    keyboard.c and keyboard.h file created.

--- kmain.c
+++ kmain.c
@@ -2,15 +2,25 @@
 #include "framebuffer.h"
 #include "serial.h"
 #include "memory_segments.h"
+#include "keyboard.h"
+#include "interrupts.h"
 
 
     
 void kmain(){
 
-    char msg[] = "KleinOS";
+    //char msg[] = "KleinOS";
+    unsigned char scancode,ascii;
+    char asciicode[1];
    
     segments_install_gdt();
-    serial_write(0x3F8, msg, sizeof(msg));
-    fb_write(msg, sizeof(msg));
+    //serial_write(msg, sizeof(msg));
+    //fb_write(msg, sizeof(msg));
+    interrupts_install_idt();
+    scancode = keyboard_read_scan_code();
+    ascii = keyboard_scan_code_to_ascii(scancode);
+    asciicode[0] = ascii;
+    serial_write(asciicode, sizeof(asciicode));
+    //fb_write(asciicode, sizeof(asciicode));
     
 }
