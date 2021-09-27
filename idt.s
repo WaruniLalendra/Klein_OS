dfc3470e8f33846c2542c154bd9f03b9a91fdd86
Waruni Lalendra <73272972+WaruniLalendra@users.noreply.github.com>
2021-09-27 10:58:04 -0700

    idt.s file updated.

--- idt.s
+++ idt.s
@@ -5,7 +5,14 @@ global  load_idt
 ;        [esp    ] the return address
 
 load_idt:
+        mov ss, [esp + 16]         ; the stack segment selector we want for user mode
+        move esp, [esp + 12]       ; the user mode stack pointer
+        mov eflags, [esp +  8]     ; the control flags we want to use in user mode
+        mov cs, [esp +  4]         ; the code segment selector
+        mov eip, [esp +  0]        ; the instruction pointer of user mode code to execute
         mov eax, [esp + 4]
         lidt [eax]
         ret
+
+
                  
